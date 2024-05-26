71fb70e882f215338be4420be465e66036b108e3
杉杉33 <105534892+OokokioO8456@users.noreply.github.com>
2024-05-26 13:10:39 +0800

    Add files via upload

---
+++ requirements-install.cmd
@@ -0,0 +1,69 @@
+@echo off
+chcp 65001
+color 09
+title 必要插件安裝中請稍後...
+
+cls
+
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo        ::::::::::. :::      ...    :::  .,-:::::/  ::::::.    :::.                                
+echo         `;;;```.;;;;;;      ;;     ;;;,;;-'````'   ;;;`;;;;,  `;;;                                
+echo          `]]nnn]]' [[[     [['     [[[[[[   [[[[[[/[[[  [[[[[. '[[                                
+echo           $$$""    $$'     $$      $$$"$$c.    "$$ $$$  $$$ "Y$c$$                                
+echo           888o    o88oo,.__88    .d888 `Y8bo,,,o88o888  888    Y88                                
+echo           YMMMb   """"YUMMM "YmmMMMM""   `'YMUP"YMMMMM  MMM     YM                                
+echo        ::::::.    :::. .::::::.:::::::::::::::.      :::      :::     ::::::.    :::.  .,-:::::/  
+echo        ;;;`;;;;,  `;;;;;;`    `;;;;;;;;'''';;`;;     ;;;      ;;;     ;;;`;;;;,  `;;;,;;-'````'   
+echo        [[[  [[[[[. '[['[==/[[[[,    [[    ,[[ '[[,   [[[      [[[     [[[  [[[[[. '[[[[[   [[[[[[/
+echo        $$$  $$$ "Y$c$$  '''    $    $$   c$$$cc$$$c  $$'      $$'     $$$  $$$ "Y$c$$"$$c.    "$$ 
+echo        888  888    Y88 88b    dP    88,   888   888,o88oo,.__o88oo,.__888  888    Y88 `Y8bo,,,o88o
+echo        MMM  MMM     YM  "YMmMY"     MMM   YMM   ""` """"YUMMM""""YUMMMMMM  MMM     YM   `'YMUP"YMM
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+
+pip install -r requirements.txt
+
+cls
+
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo      ::::::::::. :::      ...    :::  .,-:::::/  ::::::.    :::.                                          
+echo       `;;;```.;;;;;;      ;;     ;;;,;;-'````'   ;;;`;;;;,  `;;;                                          
+echo        `]]nnn]]' [[[     [['     [[[[[[   [[[[[[/[[[  [[[[[. '[[                                          
+echo         $$$""    $$'     $$      $$$"$$c.    "$$ $$$  $$$ "Y$c$$                                          
+echo         888o    o88oo,.__88    .d888 `Y8bo,,,o88o888  888    Y88                                          
+echo         YMMMb   """"YUMMM "YmmMMMM""   `'YMUP"YMMMMM  MMM     YM                                          
+echo      ::::::.    :::. .::::::.:::::::::::::::.      :::         :::::::-.      ...   :::.    :::..,::::::  
+echo      ;;;`;;;;,  `;;;;;;`    `;;;;;;;;'''';;`;;     ;;;          ;;,   `';, .;;;;;;;.`;;;;,  `;;;;;;;''''  
+echo      [[[  [[[[[. '[['[==/[[[[,    [[    ,[[ '[[,   [[[          `[[     [[,[[     \[[,[[[[[. '[[ [[cccc   
+echo      $$$  $$$ "Y$c$$  '''    $    $$   c$$$cc$$$c  $$'           $$,    $$$$$,     $$$$$$ "Y$c$$ $$""""   
+echo      888  888    Y88 88b    dP    88,   888   888,o88oo,.__      888_,o8P'"888,_ _,88P888    Y88 888oo,__ 
+echo      MMM  MMM     YM  "YMmMY"     MMM   YMM   ""` """"YUMMM      MMMMP"`    "YMMMMMP" MMM     YM """"YUMMM
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+echo ㅤ 
+pause
